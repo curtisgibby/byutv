9ed7cac2da8c22324b89c31e50fe04831d96e35f
Curtis Gibby <curtis@soundconcepts.com>
2017-10-03 09:57:30 -0600

    update to use new APIs, accommodate changes in BYU TV site;

--- download-and-resize-episode-image.js
+++ download-and-resize-episode-image.js
@@ -1,30 +1,83 @@
+var chalk = require('chalk');
 var fs = require('fs');
+var moment = require('moment-timezone');
 var request = require('request');
-var sharp = require('sharp');
 var sanitize = require('sanitize-filename');
+var sharp = require('sharp');
 
 require('./getShowGuidFromTitle.js').getShowGuidFromTitle(getApiDataForShow);
 
 function getApiDataForShow(showGuid) {
-  var url = 'http://www.byutv.org/api/Television/GetShowEpisodesByDate?context=Android%24US%24Release&showGuid=' + showGuid;
+  var url = 'https://api.byutv.org/api3/page/getpage?pageid=' + showGuid;
 
-  request(url, function(err, resp, body){
+  var options = {
+    url: url,
+    headers: {
+      'x-byutv-context': 'web$US',
+      'x-byutv-platformkey': 'xsaaw9c7y5',
+      'content-type': 'application/json',
+    }
+  };
+  request(options, function(err, resp, body){
     if (!err && resp.statusCode == 200) {
       var jsonContent = JSON.parse(body);
-      var i = 0;
+      var mostRecentSeason = false;
+      lists = jsonContent.lists;
+      Object.keys(lists).forEach(function(key) {
+        list = lists[key];
+        if (list.type == 'ShowSeason' && mostRecentSeason == false) {
+          mostRecentSeason = true;
+          getApiDataForSeason(list.id);
+        }
+      });
+    }
+  });
+}
+
+function getApiDataForSeason(seasonid) {
+  var url = 'https://api.byutv.org/api3/list/getlistitems?start=0&limit=20&channel=byutv&listid=' + seasonid;
+
+  var options = {
+    url: url,
+    headers: {
+      'x-byutv-context': 'web$US',
+      'x-byutv-platformkey': 'xsaaw9c7y5',
+      'content-type': 'application/json',
+    }
+  };
+
+  request(options, function(err, resp, body){
+    if (!err && resp.statusCode == 200) {
+      var jsonContent = JSON.parse(body);
+
+      var i = 1;
       if (process.argv.length >= 4) {
         i = parseInt(process.argv[3]);
       }
 
-      var episode = jsonContent[i];
-      var originalFilename = sanitize(episode.name) + '.original.jpg';
-      downloadFile(episode.largeImage, originalFilename, function(){
-        var outputFilename = sanitize(episode.name) + '.400x225.jpg';
+      var episode = jsonContent.items[i - 1];
+      var airdate = moment(episode.premiereDate).tz("America/Denver").format('YYYY-MM-DD HH:mm:ss');
+
+      console.log(chalk.blue.bold("\n----------------------------------------- EPISODE -----------------------------------------"));
+      console.log(chalk.green("\nname\n----"));
+      console.log(episode.subtitle);
+      console.log(chalk.green("\ndescription\n-----------"));
+      console.log(episode.description);
+      console.log(chalk.green("\nairdate\n-------"));
+      console.log(airdate);
+
+      var images = episode.images[0].images;
+      Object.keys(images).forEach(function(key) {
+        image = images[key]; // ends up with the largest one
+      });
+
+      var originalFilename = sanitize(episode.subtitle) + '.' + image.size + '.jpg';
+      downloadFile(image.url, originalFilename, function(){
+        var outputFilename = sanitize(episode.subtitle) + '.400x225.jpg';
         sharp(originalFilename)
           .resize(400, 225)
-          .toFile(outputFilename, function(err) {
-            fs.unlink(originalFilename);
-          });
+          .toFile(outputFilename, function(err) {});
+        console.log(chalk.green("\nimage\n-----"));
         console.log("saved file to", outputFilename);
       });
     }
--- scrape.js
+++ scrape.js
@@ -1,36 +1,32 @@
-var fs = require('fs');
+var chalk = require('chalk');
 var moment = require('moment-timezone');
 var request = require('request');
-var $ = require('cheerio')
-var chalk = require('chalk');
 
 require('./getShowGuidFromTitle.js').getShowGuidFromTitle(scrape);
 
 function scrape(showGuid) {
-  var url = 'http://www.byutv.org/show/' + showGuid + '?airdates=true';
+  var url = 'http://www.byutv.org/api/Television/GetShowEpisodesByDate?context=Android%24US%24Release&showGuid=' + showGuid;
 
-  request(url, function(err, resp, html){
-    if (err) return console.error(err)
-    var $html = $.load(html)
+  request(url, function(err, resp, body){
+    if (!err && resp.statusCode == 200) {
+      var jsonContent = JSON.parse(body);
 
-    $html('section.airdates-widget section.view section').map(function(i, episode) {
-      // console.log("episode : ", episode); //debug!
-      var title = $(episode).find('h1').first().text().replace(/\s\s+/g, ' ').trim();
-      var description = $(episode).find('p').attr('title');
-      // var description = $(episode).find('p').attr('title').replace(/\s\s+/g, ' ').trim();
-      if (typeof description == 'undefined') {
-        return false;
+      length = 10;
+      if (jsonContent.length < length) {
+        length = jsonContent.length;
       }
-      description = description.replace(/«/g, "'").trim();
-      var airdate = $(episode).find('time').first().attr('datetime');
-      airdate = moment(airdate).tz("America/Denver").format('YYYY-MM-DD HH:mm:ss');
-      console.log(chalk.blue.bold("\n----------------------------------------- EPISODE -----------------------------------------"));
-      console.log(chalk.green("\ntitle\n-----"));
-      console.log(title);
-      console.log(chalk.green("\ndescription\n-----------"));
-      console.log(description);
-      console.log(chalk.green("\nairdate\n-------"));
-      console.log(airdate);
-    })
+      for ( var i = 0; i < length; i++) {
+        var episode = jsonContent[i];
+
+        var airdate = moment(episode.premiereDate).tz("America/Denver").format('YYYY-MM-DD HH:mm:ss');
+        console.log(chalk.blue.bold("\n----------------------------------------- EPISODE -----------------------------------------"));
+        console.log(chalk.green("\nname\n----"));
+        console.log(episode.name);
+        console.log(chalk.green("\ndescription\n-----------"));
+        console.log(episode.description);
+        console.log(chalk.green("\nairdate\n-------"));
+        console.log(airdate);
+      }
+    }
   });
 }
